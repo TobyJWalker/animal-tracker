e1270033017a78d0a5fb7d04facd1b3e7badc2e5
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-27 23:40:17 +0100

    Added https context

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ if __name__ == "__main__":
     if os.environ.get('APP_ENV') == 'test':
         app.secret_key = os.urandom(16)
         app.run(debug=True, port=int(os.environ.get('PORT', 7474)))
+    else:
+        app.secret_key = os.urandom(16)
+        app.run(debug=False, host='0.0.0.0', port=7474, ssl_context='adhoc')
