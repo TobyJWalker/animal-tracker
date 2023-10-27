42a33ba7c449aae743c0586150a86906b0ae556d
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-27 23:57:49 +0100

    Create db tables on run if not created

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, request, render_template, session, redirect
 from lib.models import *
 
+# create the app and configure it
 app = Flask(__name__)
+create_db_tables()
 
+
+
+
+
+
+
+# run the app if file is executed
 if __name__ == "__main__":
     if os.environ.get('APP_ENV') == 'test':
         app.secret_key = os.urandom(16)
