7de9a17efb67af82b4aa3980f149a89a6136b7aa
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-27 23:14:36 +0100

    Base flask app with testing server

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, session, redirect
+import os, ssl, smtplib
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    if os.environ.get('APP_ENV') == 'test':
+        app.secret_key = os.urandom(16)
+        app.run(debug=True, port=int(os.environ.get('PORT', 7474)))
