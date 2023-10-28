58b24b939cf4f9bae2aea6d2a42edbbd84a0d3f0
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-28 15:46:39 +0100

    Updated base database test

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,6 +5,3 @@ def test_create_db_tables():
     assert User.table_exists() == True
     assert Animal.table_exists() == True
     assert Notes.table_exists() == True
-    assert User.select().count() == 0
-    assert Animal.select().count() == 0
-    assert Notes.select().count() == 0
