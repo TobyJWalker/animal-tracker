2bc0aada0057e0e44ddd07859406ea131208c880
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-29 20:25:56 +0000

    Added some test notes

--- seeds/seed_db.py
+++ seeds/seed_db.py
@@ -15,3 +15,6 @@ def seed_test_data():
 
     Animal.create(name='ringo', owner=1, species='bird', age=1, date_of_birth='2020-01-01', weight=1.0, height=1.0, length=1.0,)
     Animal.create(name='ralph', owner=1, species='dog', age=1, date_of_birth='2020-01-01', weight=1.0, height=1.0, length=1.0,)
+
+    Notes.create(content='test note 1', animal=1)
+    Notes.create(content='test note 2', animal=1)
