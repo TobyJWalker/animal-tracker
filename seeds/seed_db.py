44d7b0f07da47ce08eba48d1d3f35b72054f5766
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-30 22:52:49 +0000

    Made some seed data vary for animals

--- seeds/seed_db.py
+++ seeds/seed_db.py
@@ -13,7 +13,7 @@ def seed_test_data():
 
     User.create(username='test', email='test@gmail.com', password=sha256('@Test123'.encode()).hexdigest())
 
-    Animal.create(name='ringo', owner=1, species='bird', age=1, date_of_birth='2020-01-01', weight=1.0, height=1.0, length=1.0,)
+    Animal.create(name='ringo', owner=1, species='bird', age=10, date_of_birth='2020-01-01', weight=20.0, height=1.0, length=1.0,)
     Animal.create(name='ralph', owner=1, species='dog', age=1, date_of_birth='2020-01-01', weight=1.0, height=1.0, length=1.0,)
 
     Notes.create(content='test note 1', animal=1)
